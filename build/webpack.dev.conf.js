5b540cd6e4bed55d48b560e47cde9e202f881484
wangdanfeng <1204738681@qq.com>
2019-08-02 13:01:08 +0800

    解决冲突

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -1,16 +1,10 @@
 const path = require('path')
 const webpackBaseConf = require('./webpack.base.conf')
 const merge = require('webpack-merge')
-<<<<<<< HEAD
 const config = require('../config')
 const devConfig = {
   mode: 'development',
   devtool: config.dev.devtool,
-=======
-
-const devConfig = {
-  mode: 'development',
->>>>>>> ea2b48b853e789dcbdd05620d40fd4ccd3131e07
   devServer: {
     contentBase: './dist',
     port: 8080,
